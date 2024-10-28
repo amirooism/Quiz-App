673ee448be59e54d031b3d06ded304e853554faa
amiro hatami <amirohatami@amiros-Laptop.local>
2024-10-28 09:23:52 +0330

    add an shuffle array function name  fisher-Yates and use it to shuffle my answer and show finish quiz by if statement and I should shuffeanswers after the if statmnet so the app would not crash

--- src/componenets/Quiz.jsx
+++ src/componenets/Quiz.jsx
@@ -1,27 +1,50 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizIsCompleteImg from '../assets/quiz-complete.png';
+
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
       return [...prevAnswers, selectedAnswer];
     });
   }
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  }
+  //   QUESTIONS[activeQuestionIndex].answers.sort(() => Math.random() -0.5) , shuffele array function which i use is Fisher-Yates Shuffle
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizIsCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Complete</h2>
+      </div>
+    );
+  }
+  const shuffeldAnswers = shuffleArray(QUESTIONS[activeQuestionIndex].answers);
+
   return (
     <div id="quiz">
-<div id="question">
-      <p>{QUESTIONS[activeQuestionIndex].text}</p>
-      <ul id="answer">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div id="question">
+        <p>{QUESTIONS[activeQuestionIndex].text}</p>
+        <ul id="answer">
+          {shuffeldAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-    
 }
