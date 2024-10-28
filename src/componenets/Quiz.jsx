abaaa7d83c56fd4c96f29a0da0041b5a517f825e
amiro hatami <amirohatami@amiros-Laptop.local>
2024-10-28 10:26:48 +0330

    i wrap useCallback around handleSelects, because as we know a function is a value, and every time Quiz component is reValuted a new handleselectanswwer is evaluted so all the use effects in QuestionTimer componenet we have reEvalueted. I create a handleSkipAnswer by useinh handleSelectAnswet to null :) and wrap it with callBack too

--- src/componenets/Quiz.jsx
+++ src/componenets/Quiz.jsx
@@ -1,25 +1,22 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import QUESTIONS from "../questions";
 import quizIsCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from "./QuestionTimer";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+  
 
-  function handleSelectAnswer(selectedAnswer) {
+const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
       return [...prevAnswers, selectedAnswer];
     });
-  }
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-    }
-    return array;
-  }
-  //   QUESTIONS[activeQuestionIndex].answers.sort(() => Math.random() -0.5) , shuffele array function which i use is Fisher-Yates Shuffle
+  },[])
+  
+  
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])  
 
   if (quizIsComplete) {
     return (
@@ -29,11 +26,23 @@ export default function Quiz() {
       </div>
     );
   }
+
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  }
+  //   QUESTIONS[activeQuestionIndex].answers.sort(() => Math.random() -0.5) , shuffele array function which i use is Fisher-Yates Shuffle
+
   const shuffeldAnswers = shuffleArray(QUESTIONS[activeQuestionIndex].answers);
 
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer}/>
         <p>{QUESTIONS[activeQuestionIndex].text}</p>
         <ul id="answer">
           {shuffeldAnswers.map((answer) => (
