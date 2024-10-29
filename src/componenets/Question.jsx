ac4951c214952cc84294cffff3b83d407f235604
amiro hatami <amirohatami@amiros-Laptop.local>
2024-10-29 08:28:43 +0330

    i create a timer varibale with some condition to change the timer when an answer selected, add ke key prop to QuestionTimer jsx so it can reset the component so inner interval rest to new timer if an answer selected, and make an condition for skip answer prop for stop it skiping answer everytime and just work for the time a qustion is unselected

--- src/componenets/Question.jsx
+++ src/componenets/Question.jsx
@@ -2,20 +2,27 @@ import { useState } from "react";
 import Answers from "./Answers";
 import QuestionTimer from "./QuestionTimer";
 import QUESTION from "../questions";
-export default function Question({
-  index,
-  onselectedAnswer,
-  onSkipAnswer,
-}) {
+export default function Question({ index, onselectedAnswer, onSkipAnswer }) {
   const [answer, setAnswer] = useState({
     selectedAnswer: "",
     isCorrect: null,
   });
+
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
       isCorrect: null,
     });
+
     setTimeout(() => {
       setAnswer({
         selectedAnswer: answer,
@@ -23,20 +30,25 @@ export default function Question({
       });
 
       setTimeout(() => {
-        onselectedAnswer(answer)
+        onselectedAnswer(answer);
       }, 2000);
     }, 1000);
   }
   let answerState = "";
   if (answer.selectedAnswer && answer.isCorrect !== null) {
-    answerState = answer.isCorrect ? 'correct' : 'wrong'
+    answerState = answer.isCorrect ? "correct" : "wrong";
   } else if (answer.selectedAnswer) {
-    answerState = 'answered'
+    answerState = "answered";
   }
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null} // this skip a question every time component reset, and i dont want that, i just want to every time the aswer was unselected this work so ...
+        mode={answerState}
+        key={timer} // with this key prop here , when ever time is change, we recreate Question bar and we force the inner interval to recreated
+      />
       <p>{QUESTION[index].text}</p>
       <Answers
         answers={QUESTION[index].answers}
